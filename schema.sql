099a230aeff941c5eda588a5e41f23a720796874
Sarah Heavner <sheavner@gmail.com>
2020-11-08 16:08:05 -0700

    created employees table and imported csv

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE employees(
+	emp_no INT PRIMARY KEY NOT NULL,
+	emp_title_id VARCHAR(10),
+	birth_date DATE,
+	first_name VARCHAR,
+	last_name VARCHAR,
+	SEX VARCHAR,
+	hire_date DATE);
+	
+SELECT * FROM employees;

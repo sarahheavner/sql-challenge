d42080d0afcaafc07cc807219718c80ee12ded69
Sarah Heavner <sheavner@gmail.com>
2020-11-08 18:10:52 -0700

    added code for question 2

--- query.sql
+++ query.sql
@@ -11,6 +11,29 @@ FROM employees e
 LEFT JOIN salaries s
 ON e.emp_no = s.emp_no;
 
+--List first name, last name, and hire date for employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employees
+WHERE hire_date BETWEEN '1986-01-01' AND '1986-12-31';
+
+
+--List the manager of each department with the following information: 
+--department number, department name, the manager's employee number, last name, first name.
 
 
 
+--List the department of each employee with the following information: 
+--employee number, last name, first name, and department name.
+
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+
+
+--List all employees in the Sales department, 
+--including their employee number, last name, first name, and department 
+
+
+--List all employees in the Sales and Development departments,
+--including their employee number, last name, first name, and department name.
+
+--In descending order, list the frequency count of employee last names, i.e., how many employees share each last name

125423db09007eb5d175ca7c3b4022be978fb817
Sarah Heavner <sheavner@gmail.com>
2020-11-08 17:00:43 -0700

    dropped original table, created titles table first and then recreated employees table to use emp_title_id as foreign key

--- schema.sql
+++ schema.sql
@@ -1,10 +1,23 @@
+--Create table for titles, set title_id as Primary Key
+CREATE TABLE titles(
+	title_id VARCHAR(10) PRIMARY KEY NOT NULL,
+	title VARCHAR (25)
+);
+
+SELECT * FROM titles;
+
+--Create table for employees, set emp_no as PK and emp_title_id as Foreign Key
 CREATE TABLE employees(
 	emp_no INT PRIMARY KEY NOT NULL,
-	emp_title_id VARCHAR(10),
+	emp_title_id VARCHAR(10) NOT NULL,
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
 	birth_date DATE,
 	first_name VARCHAR,
 	last_name VARCHAR,
 	SEX VARCHAR,
-	hire_date DATE);
+	hire_date DATE
+);
 	
 SELECT * FROM employees;
+
+

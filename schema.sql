77b9a780e1bd019100d76265b01fb60e30bcc8c5
Sarah Heavner <sheavner@gmail.com>
2020-11-10 17:43:20 -0700

    changed dept_emp and dept_manager to junction tables

--- schema.sql
+++ schema.sql
@@ -40,7 +40,8 @@ CREATE TABLE dept_manager(
 	dept_no VARCHAR(5) NOT NULL,
 	emp_no INT NOT NULL,
 	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
-	FOREIGN KEY (emp_no) REFERENCES employees(emp_no)
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	PRIMARY KEY (dept_no, emp_no)
 );
 
 SELECT * FROM dept_manager;
@@ -50,8 +51,10 @@ CREATE TABLE dept_emp(
 	emp_no INT NOT NULL,
 	dept_no VARCHAR(5) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
-	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no),
+	PRIMARY KEY (emp_no, dept_no)
 );
 
 SELECT * FROM dept_emp;
+
 
